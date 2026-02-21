7cacb43ebe8ffc634a794c344ccfbd31d43e41b0
GustavoAb18 <abiliogustavo18@gmail.com>
2026-02-21 09:55:18 -0300

    first commit

---
+++ randomcode.rb
@@ -0,0 +1,16 @@
+letters = ('a'..'z').to_a
+numbers = ('0'..'9').to_a
+special = %w(! @ # $ % & * ( ) + { })
+
+pool = letters + numbers + special
+
+senha = ""
+
+puts "Quantas vezes você quer que o código repita?"
+lenght_choosed = gets.chomp.to_i
+
+lenght_choosed.times do
+  senha << pool.sample
+end
+
+puts senha
